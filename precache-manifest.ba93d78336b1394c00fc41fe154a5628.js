481ca40d6e20b318d672ba8363dc536c44fcf8d6
zabity <scruel@o2.pl>
2019-08-29 16:20:33 +0200

    Updates

---
+++ precache-manifest.ba93d78336b1394c00fc41fe154a5628.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1485add0f3fff78ed425fba4b1958840",
+    "url": "/mtg_library/index.html"
+  },
+  {
+    "revision": "12e8f73c7e70402de87a",
+    "url": "/mtg_library/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f55dfd47a4bb34c8b418",
+    "url": "/mtg_library/static/js/2.a4782117.chunk.js"
+  },
+  {
+    "revision": "12e8f73c7e70402de87a",
+    "url": "/mtg_library/static/js/main.aee643f0.chunk.js"
+  },
+  {
+    "revision": "bbb8436a96c7edc29d2d",
+    "url": "/mtg_library/static/js/runtime~main.3cf34bda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mtg_library/static/media/logo.5d5d9eef.svg"
+  }
+]);
